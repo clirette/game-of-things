7ee354d45e0dbc3b85a56a55af2dcb132bbdb581
Chase Lirette <chaselirette@yahoo.com>
2019-02-25 16:53:02 -0600

    added favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+const favicon = require('serve-favicon');
 const mongojs = require('mongojs');
 const db = mongojs('things', ['answers', 'players']);
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 app.use(logger('dev'));
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
